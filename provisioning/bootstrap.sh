28cc0b738c3273175d528f3868cf4daeaa5bf789
githubfoam <lequestionis@gmail.com>
2020-10-17 12:51:22 +0300

    reinit

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-# set -o errexit
-# set -o pipefail
-# set -o nounset
-# set -o xtrace
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
 # set -eox pipefail #safety for script
 
   echo "===================================================================================="
